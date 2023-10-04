a48398217275f0b7113b1844986a3af297d30925
Victor Hugo <vhuugoc@gmail.com>
2023-10-04 07:41:06 -0300

    Validations no produto e material

---
+++ src/main/java/com/starglass/api/domain/product/ProductValidator.java
@@ -0,0 +1,50 @@
+package com.starglass.api.domain.product;
+
+import com.google.common.collect.Lists;
+import com.querydsl.core.BooleanBuilder;
+import com.starglass.api.domain.user.User;
+import com.starglass.api.infra.entity.EntityValidator;
+import com.starglass.api.infra.exception.custom.ValidationException;
+import com.starglass.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductValidator implements EntityValidator<Product> {
+
+    private final ProductRepository productRepository;
+
+    private final TokenService tokenService;
+
+    @Autowired
+    public ProductValidator(ProductRepository productRepository, TokenService tokenService) {
+        this.productRepository = productRepository;
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public void validate(Product entity) throws ValidationException {
+        this.validateNotExists(entity);
+    }
+
+    public void validateNotExists(Product entity) {
+        User authenticatedUser = tokenService.getAuthenticatedUserFromContext();
+        boolean isUpdate = entity.getId() != null;
+
+        QProduct qProduct = QProduct.product;
+        BooleanBuilder queryBuilder = new BooleanBuilder();
+
+        queryBuilder.and(qProduct.merchant.eq(authenticatedUser.getMerchant()));
+        queryBuilder.and(qProduct.name.eq(entity.getName()));
+
+        if (isUpdate) queryBuilder.and(qProduct.id.ne(entity.getId()));
+
+        List<Product> list = Lists.newArrayList(this.productRepository.findAll(queryBuilder));
+
+        if (!list.isEmpty()) throw new ValidationException("This product already exists");
+
+    }
+
+}
--- src/main/java/com/starglass/api/domain/product/material/MaterialValidator.java
+++ src/main/java/com/starglass/api/domain/product/material/MaterialValidator.java
@@ -1,14 +1,51 @@
 package com.starglass.api.domain.product.material;
 
+import com.google.common.collect.Lists;
+import com.querydsl.core.BooleanBuilder;
+import com.starglass.api.domain.user.User;
 import com.starglass.api.infra.entity.EntityValidator;
 import com.starglass.api.infra.exception.custom.ValidationException;
+import com.starglass.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MaterialValidator implements EntityValidator<Material> {
 
+    private final MaterialRepository materialRepository;
+
+    private final TokenService tokenService;
+
+    @Autowired
+    public MaterialValidator(MaterialRepository materialRepository, TokenService tokenService) {
+        this.materialRepository = materialRepository;
+        this.tokenService = tokenService;
+    }
+
     @Override
     public void validate(Material entity) throws ValidationException {
+        this.validateNotExists(entity);
+    }
+
+    public void validateNotExists(Material entity) {
+        User authenticatedUser = tokenService.getAuthenticatedUserFromContext();
+        boolean isUpdate = entity.getId() != null;
+
+        QMaterial qMaterial = QMaterial.material;
+        BooleanBuilder queryBuilder = new BooleanBuilder();
+
+        queryBuilder.and(qMaterial.merchant.eq(authenticatedUser.getMerchant()));
+        queryBuilder.and(qMaterial.name.eq(entity.getName()).and(qMaterial.type.eq(entity.getType())));
+        if (entity.getColor() != null) queryBuilder.and(qMaterial.color.eq(entity.getColor()));
+        if (entity.getThickness() != null) queryBuilder.and(qMaterial.thickness.eq(entity.getThickness()));
+
+        if (isUpdate) queryBuilder.and(qMaterial.id.ne(entity.getId()));
+
+        List<Material> list = Lists.newArrayList(this.materialRepository.findAll(queryBuilder));
+
+        if (!list.isEmpty()) throw new ValidationException("This material already exists");
 
     }
 
